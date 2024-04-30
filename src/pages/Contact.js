ade15b7c67b8a9341856738a9512873c1a974d60
alemaria23 <alexandraterecoasa23@gmail.com>
2024-04-30 13:45:24 +0300

    First Commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,12 +10,13 @@ export default function Contact() {
       <NavbarComponent props={"3"} />
       <MDBContainer fluid className="body">
       <br /><br /><br />
-        <h1>Hei!</h1>
-        <h4>Nu ai reusit sa gasesti informatiile de care aveai nevoie?</h4>
-        <h5>Nu iti face griji. Suntem aici ca sa te ajutam.</h5>
-        <p>
-           Da-ne un email la adresa ... , iar noi vom reveni cu un mesaj in cel mai scurt timp!
+        <h1 className="centrare">Hei!</h1>
+        <h4 className="centrare">Nu ai reusit sa gasesti informatiile de care aveai nevoie?</h4>
+        <h5 className="centrare">Nu iti face griji. Suntem aici ca sa te ajutam.</h5>
+        <p className="centrare">
+           Trimite un ticket pe platforma <a href="https://ticket.unitbv.ro/?lang=ro">Ticket UNITBV</a> si cineva te v-a ajuta in cel mai scurt timp posibil.
         </p>
+        <p className="centrare">Daca ai dificultati sa folosesti platforma, poti intra in sectiunea "Ticket" din cadrul acestei platforme pentru a te informa.</p>
       </MDBContainer>
       <FooterComponent />
     </>
